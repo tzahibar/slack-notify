ff0cf3a01d5fcd5e594491ea99aa394d6025b310
Tzahi Bar <tzahib@jfrog.com>
2024-06-05 15:26:39 +0300

    Slack - 26

--- slack/slack.js
+++ slack/slack.js
@@ -1,3 +1,22 @@
 const name = 'slack';
+const axios = require('axios');
 
 console.log(`Hello from ${name}!`);
+
+const url = 'https://hooks.slack.com/services/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';
+const data = {
+    text: 'Hello, World!',
+};
+
+axios.post(url, data, {
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+    .then(response => {
+        console.log('Message sent:', response.data);
+    })
+    .catch(error => {
+        console.error('Error sending message:', error);
+    });
+
