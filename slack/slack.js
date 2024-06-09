abdd8fe47321de80329308802461fc928ef1d9ca
Tzahi Bar <tzahib@jfrog.com>
2024-06-09 15:59:04 +0300

    Slack - 47

--- slack/slack.js
+++ slack/slack.js
@@ -8,8 +8,8 @@ const service_name = process.argv[3];
 const run_number = process.argv[4];
 const branch_name = process.argv[5];
 
-console.log(`service_name: ${service_name}`);
-const message = `:white_check_mark: \n\`\`\`${service_name} release workflow finised successfully :white_check_mark:\nSummary:\`\`\`:\n*Run:*<https://www.ynet.co.il|Run number ${run_number}>\n\n*Branch:*\n${branch_name}\n\n *Released Services:*\n${service_name}`;
+const message = `:white_check_mark: \`${service_name} release workflow\`finished successfully :white_check_mark:\n\nSummary:\n*Run:* <https://www.ynet.co.il|Run number ${run_number}>\n\n*Branch:* ${branch_name}\n\n*Released Services:* ${service_name}`;
+
 console.log(message);
 
 async function sendSlackNotification(channel, text) {
