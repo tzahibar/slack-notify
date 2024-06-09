f3ea1776cd89ea7194e7b701b83f9f586273b9b6
Tzahi Bar <tzahib@jfrog.com>
2024-06-09 14:29:23 +0300

    Slack - 35

--- slack/slack.js
+++ slack/slack.js
@@ -4,11 +4,11 @@ const { WebClient } = require('@slack/web-api');
 const token = process.argv[2];
 const web = new WebClient(token);
 
-const workflow_name = process.argv[3];
+const service_name = process.argv[3];
 const run_number = process.argv[4];
-const branch_name = process.argv[5];
 
-const message = `${workflow_name} run number: ${run_number} finished on branch ${branch_name}`;
+
+const message = `:white_check_mark: ${service_name} release summary:\nrun ${run_number} finished successfully on\n${branch_name}\n\n *Released Services:*\n${service_name}`;
 console.log(message);
 
 async function sendSlackNotification(channel, text) {
