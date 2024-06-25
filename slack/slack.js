fadd095e7cb825b9ba085f63070966005a97dc95
Tzahi Bar <tzahib@jfrog.com>
2024-06-25 15:09:16 +0300

    Slack - 116

--- slack/slack.js
+++ slack/slack.js
@@ -8,21 +8,23 @@ const service_name = process.argv[3];
 const run_number = process.argv[4];
 const branch_name = process.argv[5];
 
-const message = `:white_check_mark: \`${service_name} release workflow\` :white_check_mark:\n*Status:*\nSuccessful\n\n*Run:*\n<https://www.ynet.co.il|Run number ${run_number}>\n\n*Branch:*\n${branch_name}\n\n*Released Services:*\n${service_name}`;
+// const message = `:white_check_mark: \`${service_name} release workflow\` :white_check_mark:\n*Status:*\nSuccessful\n\n*Run:*\n<https://www.ynet.co.il|Run number ${run_number}>\n\n*Branch:*\n${branch_name}\n\n*Released Services:*\n${service_name}`;
+//
+// console.log(message);
+//
+// async function sendSlackNotification(channel, text) {
+//     try {
+//         const result = await web.chat.postMessage({
+//             channel: channel,
+//             text: text,
+//         });
+//
+//         console.log(`Successfully sent message ${result.ts} in conversation ${channel}`);
+//     } catch (error) {
+//         console.error(`Error posting message: ${error}`);
+//     }
+// }
+//
+// sendSlackNotification('#tzahi', message).then(r => console.log(r))
 
-console.log(message);
-
-async function sendSlackNotification(channel, text) {
-    try {
-        const result = await web.chat.postMessage({
-            channel: channel,
-            text: text,
-        });
-
-        console.log(`Successfully sent message ${result.ts} in conversation ${channel}`);
-    } catch (error) {
-        console.error(`Error posting message: ${error}`);
-    }
-}
-
-sendSlackNotification('#tzahi', message).then(r => console.log(r))
+console.log('hi!');
